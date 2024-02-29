aa0ade0831196e5b968e4dc8f4a6b57e98b10cdd
Graceful Potato <gracefulpotatow@gmail.com>
2024-02-29 23:50:15 +0400

    Add posts update action.

--- examples/hanami_2.1/config/routes.rb
+++ examples/hanami_2.1/config/routes.rb
@@ -9,7 +9,7 @@ module Blog
         get "/posts", to: "posts.index"
         get "/posts/:id", to: "posts.show"
         post "/posts", to: "posts.create"
-        # patch "/posts/:id", to: "posts.update"
+        patch "/posts/:id", to: "posts.update"
         # delete "/posts/:id", to: "posts.destroy"
       end
     end
---
+++ examples/hanami_2.1/slices/api/actions/posts/update.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module API::Actions::Posts
+  class Update < API::Action
+    include API::Deps[
+      repo: "repositories.posts",
+      post_serializer: "serializers.post"
+    ]
+
+    params do
+      required(:id).value(:integer)
+      required(:post).hash do
+        optional(:title).value(:string)
+        optional(:body).value(:string)
+      end
+    end
+
+    def handle(request, response)
+      post = repo.find(request.params[:id])
+      halt 404, { error: "Not found" }.to_json unless post
+      halt 422, { error: request.params.errors }.to_json unless request.params.valid?
+      halt 422, { error: "Title or body should be in request." }.to_json if request.params[:post].empty?
+
+      post = repo.update(post.id, request.params[:post])
+
+      response.format = :json
+      response.status = 200
+      response.body = post_serializer.render(post)
+    end
+  end
+end
