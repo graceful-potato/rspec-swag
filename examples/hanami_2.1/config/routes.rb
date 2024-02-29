289d7de39b114a422fc07b483501f2af99a57baa
Graceful Potato <gracefulpotatow@gmail.com>
2024-02-29 23:18:08 +0400

    Add slice for API. Draft routes for post crud actions.

--- examples/hanami_2.1/config/routes.rb
+++ examples/hanami_2.1/config/routes.rb
@@ -3,5 +3,15 @@
 module Blog
   class Routes < Hanami::Routes
     # Add your routes here. See https://guides.hanamirb.org/routing/overview/ for details.
+
+    slice :api, at: "/api" do
+      scope "v1" do
+        # get "/posts", to: "posts.index"
+        # get "/posts/:id", to: "posts.show"
+        # post "/posts", to: "posts.create"
+        # patch "/posts/:id", to: "posts.update"
+        # delete "/posts/:id", to: "posts.destroy"
+      end
+    end
   end
 end
---
+++ examples/hanami_2.1/slices/api/action.rb
@@ -0,0 +1,7 @@
+# auto_register: false
+# frozen_string_literal: true
+
+module API
+  class Action < Blog::Action
+  end
+end
