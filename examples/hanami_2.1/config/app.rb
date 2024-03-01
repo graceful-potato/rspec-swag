f0db612ce38e7c9327967f7419e440bccf7bb715
Graceful Potato <gracefulpotatow@gmail.com>
2024-03-01 04:54:13 +0400

    Edit content security policy in order to use cdn swagger js script and inline scripts.

--- examples/hanami_2.1/config/app.rb
+++ examples/hanami_2.1/config/app.rb
@@ -5,5 +5,6 @@ require "hanami"
 module Blog
   class App < Hanami::App
     config.middleware.use :body_parser, :json
+    config.actions.content_security_policy[:script_src] += " https://unpkg.com/ 'unsafe-inline'"
   end
 end
